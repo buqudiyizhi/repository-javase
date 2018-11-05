9fd8de9fc2fe08d0b165ae6cfd1a0e3e725f82aa
buqudiyizhi <33833956+buqudiyizhi@users.noreply.github.com>
2018-11-05 19:30:45 +0800

    将java代码上传到GitHub中去！！！

---
+++ src/com/itcast/test/MyDemo.java
@@ -0,0 +1,11 @@
+package com.itcast.test;
+
+public class MyDemo {
+    public static void main(String[] args) {
+        int i=8;
+        for (int j = 0; j < 5; j++) {
+            System.out.println(i+j);
+        }
+
+    }
+}
